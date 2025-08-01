ea7fb8fff7f8384113d4f37d5fe49e76c380b4ec
TyrusYeo <tyrusyeo5@gmail.com>
2025-08-01 16:59:30 -0400

    new metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'The Third Place',
-  description: 'join the creative collective',
+  title: 'Rasi Cafe',
+  description: 'Sip Slowly, Stay Awhile...',
 }
 
 export default function RootLayout({ children }) {
